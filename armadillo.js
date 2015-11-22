e04023d3f421eaaa0d7efe55f24734b33be3c159
Joshua Miles <milesjos@msu.edu>
2015-11-22 02:33:01 -0600

    Added the welcome screen and the sidebar toggle

--- armadillo.js
+++ armadillo.js
@@ -2,3 +2,8 @@ $(".c-hamburger").click(function(event) {
 	$(".c-hamburger").toggleClass('is-active ham-trans');
 	$( "#sidebar" ).toggleClass('show-thing');
 });
+
+function search(){
+	$('#welcome').fadeOut('500');
+	$('.c-hamburger').css('visibility', 'visible').hide().fadeIn('500');
+}
