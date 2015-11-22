94bd4d55191f07c1279d32fd7319157503cfeddb
Joshua Miles <milesjos@msu.edu>
2015-11-22 06:06:35 -0600

    now can receive values from the search bars

--- armadillo.js
+++ armadillo.js
@@ -4,14 +4,16 @@ $(".c-hamburger").click(function(event) {
 });
 
 function searchInit(event){
-	
+	var site = $('.searchbar').val();
+
 	$('#welcome').fadeOut('100');
 	$('.c-hamburger').css('visibility', 'visible').hide().fadeIn('100');
 	event.preventDefault();
 }
 
 function searchPost(event){
-	
+	var site = $('.searchbar2').val();
+
 	$(".c-hamburger").toggleClass('is-active ham-trans');
 	$( "#sidebar" ).toggleClass('show-thing');
 	event.preventDefault();
