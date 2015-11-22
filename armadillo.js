e323643a716298f7b7ebb3c5a083dc2858d30e69
Joshua Miles <milesjos@msu.edu>
2015-11-21 21:34:35 -0600

    Added some UX stuff. Sorry for the terrible commit message- I'm exhausted.

--- armadillo.js
+++ armadillo.js
@@ -1,3 +1,11 @@
 $('#searchbar').click(function() {
 	$('#sidebar').toggleClass("active");
 });	
+
+$( "#sidebar" ).hover(
+    function() {
+    $( this ).css("transform", "translateX(0%)");
+  }, function() {
+    $( this ).css("transform", "translateX(-90%)");
+  }
+);
