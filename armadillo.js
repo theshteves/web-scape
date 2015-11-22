0a83656df24f413c204ed0c418448674c67e4125
Joshua Miles <milesjos@msu.edu>
2015-11-22 00:14:13 -0600

    Added styles and fixed the sidebar

--- armadillo.js
+++ armadillo.js
@@ -1,11 +1,4 @@
-$('#searchbar').click(function() {
-	$('#sidebar').toggleClass("active");
-});	
-
-$( "#sidebar" ).hover(
-    function() {
-    $( this ).css("transform", "translateX(0%)");
-  }, function() {
-    $( this ).css("transform", "translateX(-90%)");
-  }
-);
+$(".c-hamburger").click(function(event) {
+	$(".c-hamburger").toggleClass('is-active ham-trans');
+	$( "#sidebar" ).toggleClass('show-thing');
+});
