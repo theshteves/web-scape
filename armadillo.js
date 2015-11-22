c6d005b822ee10025e1a3ae4a437425e50878090
Joshua Miles <milesjos@msu.edu>
2015-11-22 05:42:04 -0600

    Added the preventDefaults on the search functions and added the close on searchPost function

--- armadillo.js
+++ armadillo.js
@@ -3,8 +3,16 @@ $(".c-hamburger").click(function(event) {
 	$( "#sidebar" ).toggleClass('show-thing');
 });
 
-function search(){
+function searchInit(event){
 	
 	$('#welcome').fadeOut('100');
 	$('.c-hamburger').css('visibility', 'visible').hide().fadeIn('100');
+	event.preventDefault();
+}
+
+function searchPost(event){
+	
+	$(".c-hamburger").toggleClass('is-active ham-trans');
+	$( "#sidebar" ).toggleClass('show-thing');
+	event.preventDefault();
 }
